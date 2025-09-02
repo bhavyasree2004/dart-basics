a3abb4e8075347b6e2553414c6ad0d77e27bebb8
bhavyasree2004 <bhavyasreecodes@gmail.com>
2025-09-02 15:37:07 +0530

    learning-inheritance

---
+++ inheritance_practice.dart
@@ -0,0 +1,31 @@
+class Vehicle {
+  String brand;
+  int year;
+  Vehicle(this.brand, this.year);
+  void showDetails() {
+    print('Brand: ${brand} Year: ${year}');
+  }
+}
+
+class Car extends Vehicle {
+  int seats;
+  Car(String brand, int year, this.seats) : super(brand, year);
+  void showDetails() {
+    print('Brand: ${brand} Year: ${year} Seats: ${seats}');
+  }
+}
+
+class Bike extends Vehicle {
+  String type;
+  Bike(String brand, int year, this.type) : super(brand, year);
+  void showDetails() {
+    print('Brand: ${brand} Year: ${year} Type: ${type}');
+  }
+}
+
+void main() {
+  Car car = new Car("suzuki", 2022, 4);
+  car.showDetails();
+  Bike bike = new Bike('Hero Honda', 2021, "Scooty");
+  bike.showDetails();
+}
